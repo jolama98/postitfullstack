83dc366eb56f85f2464d40948f543389121cd324
jo <josehehall98@gmail.com>
2025-04-18 13:56:27 -0600

    Created SQL tables

--- server/dbSetup.sql
+++ server/dbSetup.sql
@@ -6,3 +6,47 @@ CREATE TABLE IF NOT EXISTS accounts(
   email VARCHAR(255) UNIQUE COMMENT 'User Email',
   picture VARCHAR(255) COMMENT 'User Picture'
 ) default charset utf8mb4 COMMENT '';
+
+CREATE TABLE picture(
+  id VARCHAR(255) NOT NULL PRIMARY KEY COMMENT 'primary key',
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT 'Time Created',
+  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Last Update',
+  url VARCHAR(255)NOT NULL COMMENT 'Picture URL',
+  album_id VARCHAR(255)NOT NULL COMMENT 'Album ID',
+creator_id VARCHAR(255) NOT NULL COMMENT 'Creator ID',
+  FOREIGN KEY (creator_id) REFERENCES accounts(id) ON DELETE CASCADE ,
+  FOREIGN KEY (album_id) REFERENCES albums(id) ON DELETE CASCADE 
+)
+
+CREATE TABLE albums(
+  id VARCHAR(255) NOT NULL PRIMARY KEY COMMENT 'primary key',
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT 'Time Created',
+  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Last Update',
+  title VARCHAR(255) NOT NULL COMMENT 'Album Name',
+description VARCHAR(255) NOT NULL COMMENT 'Album Description',
+isArchived BOOLEAN NOT NULL DEFAULT false COMMENT 'Is Archived',
+category ENUM(
+    'aesthetics',
+    'games',
+    'animals',
+    'food',
+    'vibes',
+    'misc'
+),
+coverImage VARCHAR(255) NOT NULL COMMENT 'Cover Image',
+creator_id VARCHAR(255) NOT NULL COMMENT 'Creator ID',
+  FOREIGN KEY (creator_id) REFERENCES accounts(id) ON DELETE CASCADE
+)
+
+ DROP TABLE albums;
+
+
+ CREATE TABLE watcher(
+  id VARCHAR(255) NOT NULL PRIMARY KEY COMMENT 'primary key',
+created_at DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT 'Time Created',
+updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Last Update',
+account_id VARCHAR(255) NOT NULL COMMENT 'Account ID',
+album_id VARCHAR(255) NOT NULL COMMENT 'Album ID',
+  FOREIGN KEY (account_id) REFERENCES accounts(id) ON DELETE CASCADE ,
+  FOREIGN KEY (album_id) REFERENCES albums(id) ON DELETE CASCADE
+ )
